7222ed9f7a6bec7c3da285c44e8ea70895e450f1
Wesley Luk <wesleyluk@gmail.com>
2020-03-14 11:16:54 -0700

    wrap

--- build.bat
+++ build.bat
@@ -1,1 +1,5 @@
-docker run --mount type=bind,source=%cd%/keyboards,target=/qmk_firmware/keyboards --mount type=bind,source=%cd%/layout,target=/qmk_firmware/layout qmkfm/qmk_firmware /bin/bash -c "qmk compile layout/default.json && cp wesv1_default.hex layout/."
+docker run^
+    --mount type=bind,source=%cd%/keyboards,target=/qmk_firmware/keyboards^
+    --mount type=bind,source=%cd%/layout,target=/qmk_firmware/layout^
+    qmkfm/qmk_firmware^
+    /bin/bash -c "qmk compile layout/default.json && cp wesv1_default.hex layout/."
